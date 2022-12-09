9f9bd333aaa3cc1f5063217b8ea7203108aa15b4
petres <peter.reschenhofer@gmail.com>
2022-12-09 09:08:55 +0100

    fixing aggm gas cons loading

--- load/aggm/gas-consumption.r
+++ load/aggm/gas-consumption.r
@@ -4,29 +4,25 @@ source("load/aggm/_shared.r")
 
 historic.file.name = file.path(g$d$tmp, "consumption-gas-aggm-historic.csv")
 
-agg = function(d) d[, .(
-    value = sum(value)/10^9
-), by = .(date = as.Date(from))]
-
+agg = function(d, years) {
+    d[, .(
+        value = sum(value) / 10^9
+    ), by = .(date = as.Date(from))][
+        year(date) %in% years
+    ]
+}
 
 # - LOAD -----------------------------------------------------------------------
 if (!file.exists(historic.file.name)) {
     # Split data range (else would be slow or not work)
-
-    cache.path = file.path(g$d$tmp, "consumption-gas-aggm-historic-cache.csv")
-
-    d.t0 = fread(cache.path)[, date := as.Date(date)] |>
-        spread(variable, value) |>
-        dplyr::select(date, value)
-
-    #d.historic = getGasConsumption("2018-12-30", "2022-01-02")
-    #d.t0 = agg(d.historic)[year(date) %in% 2019:2021]
+    d.historic = getGasConsumption("2018-12-30", "2022-01-02")
+    d.t0 = agg(d.historic, 2019:2021)
 
     d.historic = getGasConsumption("2015-12-30", "2019-01-02")
-    d.t1 = agg(d.historic)[year(date) %in% 2016:2018]
+    d.t1 = agg(d.historic, 2016:2018)
 
     d.historic = getGasConsumption("2012-12-30", "2016-01-02")
-    d.t2 = agg(d.historic)[year(date) %in% 2013:2015]
+    d.t2 = agg(d.historic, 2013:2015)
 
     fwrite(rbind(d.t0, d.t1, d.t2)[order(date)][!is.na(value)], historic.file.name)
 
@@ -35,13 +31,13 @@ if (!file.exists(historic.file.name)) {
 
 d.historic = fread(historic.file.name)[, date := as.Date(date)]
 update.time = now()
-d.base = agg(getGasConsumption(startDate = "2022-11-20"))[order(date)]
-
-# loadPackages('ggplot2')
-# ggplot(d.base, aes(x = date, y = value)) + geom_line()
+d.base = agg(getGasConsumption(startDate = "2021-12-30"), 2022:2024)[order(date)]
 
 # Combine
-d.full = rbind(d.historic, d.base[date > max(d.historic$date)])
+d.full = rbind(d.historic, d.base)
+
+# loadPackages('ggplot2')
+# ggplot(d.full, aes(x = date, y = value)) + geom_line()
 
 
 # - STORAGE --------------------------------------------------------------------
@@ -51,4 +47,3 @@ saveToStorages(d.full, list(
     format = "csv",
     update.time = update.time
 ))
-
