48cd8917dc419b0a7fe19485429d94a0582c1784
joph <johannes.schmidt@boku.ac.at>
2023-01-12 12:42:56 +0100

    Updated gas saving code so it works in 2023

--- calc/prediction-gas-consumption/_shared.r
+++ calc/prediction-gas-consumption/_shared.r
@@ -16,12 +16,14 @@ d.base = loadBase(TRUE)[!is.na(gas.consumption) & !is.na(temp)]
 # d.base = d.base[year >= min(d.base[day == 1]$year)]
 
 max.date = d.base[max(date) == date]$date
-max.day = yday(max.date)
+max.day = day(max.date)
+max.year = year(max.date)
+max.month = month(max.date)
 
-d.gas.years = d.base[day <= max.day, .(days = .N, mean = mean(gas.consumption) * 10^3), by = year]
-mean.gas.before22 = d.gas.years[year < 2022, mean(mean)]
-d.gas.years[, rel := mean / mean.gas.before22]
-savings.2022 = 1 - d.gas.years[year == 2022, rel]
+#d.gas.years = d.base[day <= max.day, .(days = .N, mean = mean(gas.consumption) * 10^3), by = year]
+#mean.gas.before22 = d.gas.years[year < 2022, mean(mean)]
+#d.gas.years[, rel := mean / mean.gas.before22]
+#savings.2022 = 1 - d.gas.years[year == 2022, rel]
 
 
 # - MODEL ----------------------------------------------------------------------
@@ -74,42 +76,97 @@ d.comp.pred = rbind(
     l.model.power$d.plot[variable %in% c("prediction")][date >= start.date][, .(
         date, variable = "pred.power", value
     )]
-)[, .(value = sum(value)), by = .(month = month(date), variable)]
+)[, .(value = sum(value)), by = .(year = year(date), month = month(date), variable)]
 
-d.data.agg = d.base[day <= max.day & month >= 3, .(
+d.data.agg = d.base[month(date) != max.month, .(
+    variable = "observation",
     value = sum(gas.consumption)
+
 ), by = .(month = month(date), year = year(date))]
 
+d.data.agg.short = d.base[(max.month == month(date)) &
+                              (day(date) <= max.day), .(
+    variable = "observation",
+    value = sum(gas.consumption)
+    ),
+    by = .(month = month(date), year = year(date))]
+
+available.months.march = d.comp %>%
+    filter(variable == "pred.base") %>%
+    dplyr::select(month.name) %>%
+    unique()
+
+available.months.august = d.comp %>%
+    filter(variable == "pred.base") %>%
+    dplyr::select(month.name) %>%
+    unique() %>%
+    mutate(n=1:n()) %>%
+    filter(n>5)
+
 d.comp = rbind(
-    d.comp.pred,
-    d.data.agg[year %in% c.years.avg, .(variable = "avg", value = mean(value)), by = month],
-    d.data.agg[year == 2022, .(month, variable = "value.22", value)],
-    d.data.agg[year == 2021, .(month, variable = "value.21", value)]
+    d.comp.pred[ , .(year = "prediction", month, variable, value)],
+    d.data.agg[year %in% c.years.avg, .(year="2017-2021", variable = "observation", value = mean(value)), by = month],
+    d.data.agg[year == 2023, .(year = "2023", month, variable, value)],
+    d.data.agg[year == 2022  & month >=3, .(year = "2022", month, variable, value)],
+    d.data.agg[year == 2021, .(year = "2021", month, variable, value)],
+    d.data.agg.short[year %in% c.years.avg, .(year="2017-2021", variable = "observation", value = mean(value)), by = month],
+    d.data.agg.short[year == 2023, .(year = "2023", month, variable, value)],
+    d.data.agg.short[year == 2022 & month >=3, .(year = "2022", month, variable, value)],
+    d.data.agg.short[year == 2021, .(year = "2021", month, variable, value)]
 )
+
 d.comp[, month.name := as.character(month(month, label = TRUE, abbr = FALSE, locale = "de_AT.UTF-8"))]
 
+d.comp = d.comp %>%
+    mutate(year = ifelse(year %in% c("2022", "2023"), "2022-2023", year))
+
+available.months.march = d.comp %>%
+    filter(variable == "pred.base") %>%
+    dplyr::select(month.name) %>%
+    unique()
+
+available.months.august = d.comp %>%
+    filter(variable == "pred.base") %>%
+    dplyr::select(month.name) %>%
+    unique() %>%
+    mutate(n=1:n()) %>%
+    filter(n>5)
 
 d.comp.a = rbind(
-    d.comp[, .(month.name, variable, value)],
-    d.comp[month >= 3, .(month.name = "**Seit März**", value = sum(value)), by = variable],
-    d.comp[month >= 8, .(month.name = "**Seit August**", value = sum(value)), by = variable]
+    d.comp[, .(year, month.name, variable, value)],
+    d.comp %>%
+        ungroup() %>%
+        filter(month.name %in% available.months.march$month.name) %>%
+        group_by(year, variable) %>%
+        summarize(value = sum(value)) %>%
+        mutate(month.name = "**Seit März**"),
+    d.comp %>%
+        ungroup() %>%
+        filter(month.name %in% available.months.august$month.name) %>%
+        group_by(year, variable) %>%
+        summarize(value = sum(value)) %>%
+        mutate(month.name = "**Seit August**")
 )
 
 d.comp.a[, month.name := factor(
     month.name, unique(d.comp.a$month.name), unique(d.comp.a$month.name)
 )]
 
-
-d.comp.c = merge(
-    d.comp.a[variable == "value.22", .(month.name, value)],
-    d.comp.a[, .(month.name, variable, comparison = value)],
-    by = "month.name"
-)
+d.comp.c = d.comp.a %>%
+    as_tibble() %>%
+    filter(year == "2022-2023",
+           variable == "observation") %>%
+    left_join(d.comp.a, by= c("month.name")) %>%
+    mutate(comparison = value.y,
+           value = value.x) %>%
+    filter(year.y != "2022-2023") %>%
+    as.data.table()
 
 d.comp.c[, rel := value / comparison]
 d.comp.c[, g100 := round((rel - 1) * 100, 2)]
+d.comp.c = d.comp.c %>%
+    mutate(full.variable = glue('{year.y}.{variable.y}'))
 
-d.comp.f = dcast(d.comp.c, month.name ~ variable, value.var = "g100")
-d.comp.f[, value.22 := NULL]
+d.comp.f = dcast(d.comp.c, month.name ~ full.variable, value.var = "g100")
 
-setcolorder(d.comp.f, c("month.name", "value.21", "avg", "pred.base", "pred.power"))
+setcolorder(d.comp.f, c("month.name", "2021.observation", "2017-2021.observation", "prediction.pred.base", "prediction.pred.power"))
