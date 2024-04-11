ecf511ee41d252796441c77ed3ab82df126e19b0
Peter Reschenhofer <peter.reschenhofer@gmail.com>
2024-04-11 19:06:43 +0200

    fixed gas aggm

--- load/aggm/gas-consumption.r
+++ load/aggm/gas-consumption.r
@@ -46,7 +46,7 @@ if (!file.exists(historic.file.name)) {
 d.historic = fread(historic.file.name)[, date := as.Date(date)]
 
 update.time = now()
-d.base = agg(getGasConsumption(startDate = "2023-12-30"), 2023:year(Sys.Date()))[order(date)]
+d.base = agg(getGasConsumption(startDate = "2023-12-30"), 2024:year(Sys.Date()))[order(date)]
 
 d.full = rbind(d.historic, d.base) %>%
     na.omit()
