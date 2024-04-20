59625c0587b41ba828954d05f5de47c91a9ff85f
petres <peter@abteil.org>
2024-04-21 01:09:36 +0200

    Splittiedup hourly res from default loading scripts

--- load/entsoe/generation.r
+++ load/entsoe/generation.r
@@ -6,53 +6,40 @@ source("load/entsoe/_shared.r")
 
 # - LOAD/PREP ------------------------------------------------------------------
 update.time = now()
+d.base = loadEntsoeComb(
+    # type = "generation", month.start = "2022-07", month.end = "2022-07", check.updates = FALSE
+    type = "generation", month.start = "2014-12", month.end = month.end
+)
 
-d.base = loadEntsoeComb(# type = "generation", month.start = "2022-07", month.end = "2022-07", check.updates = FALSE
-    type = "generation",
-    month.start = "2014-12",
-    month.end = month.end)
+# unique(d.base$ProductionType)
 
 d.base.f = d.base[AreaTypeCode == "CTY"]
-d.base.f[,
-         hour := floor_date(DateTime, unit = "hours")]
+d.base.f[, factor := resToFactor[ResolutionCode]]
+# d.base.f[, .(sum = sum(ActualGenerationOutput)), by=.(ProductionType)][order(sum)]
+# unique(d.base.f[,. (ResolutionCode, AreaCode, AreaTypeCode, AreaName, MapCode)])
 
-
-d.agg.hours = d.base.f[, .(
-    value = mean(ActualGenerationOutput, na.rm = TRUE),
-    cons = mean(ActualConsumption, na.rm = TRUE)
-), by = .(country = MapCode,
-          source = ProductionType,
-          DateTime = hour)][order(DateTime)]
 
 # - AGG -----------------------------------------------------------------------
-d.agg = d.agg.hours[, .(value = sum(value) / 10 ^ 6,
-                        cons = sum(cons) / 10 ^ 6), by = .(country = country,
-                                                           date = as.Date(DateTime),
-                                                           source = source)][order(date)]
+d.agg = d.base.f[, .(
+    value = sum(ActualGenerationOutput*factor, na.rm = TRUE)/10^6,
+    cons = sum(ActualConsumption*factor, na.rm = TRUE)/10^6
+), by = .(
+    country = MapCode,
+    date = as.Date(DateTime),
+    source = ProductionType
+)][order(date)]
 
 # Delete last (most probably incomplete) obs
 d.agg = removeLastDays(d.agg, 2)
 
-# - STORE ----------------------------------------------------------------------
-saveToStorages(
-    d.agg.hours,
-    list(
-        id = "electricity-generation-hourly",
-        source = "entsoe",
-        format = "csv",
-        update.time = update.time
-    )
-)
 
-saveToStorages(
-    d.agg,
-    list(
-        id = "electricity-generation",
-        source = "entsoe",
-        format = "csv",
-        update.time = update.time
-    )
-)
+# - STORE ----------------------------------------------------------------------
+saveToStorages(d.agg, list(
+    id = "electricity-generation",
+    source = "entsoe",
+    format = "csv",
+    update.time = update.time
+))
 
 
 nameOthers = "others"
@@ -60,31 +47,29 @@ nameOthers = "others"
 # - GROUP 1
 addGroupCol(d.agg, c.sourceGroups1, nameOthers = nameOthers)
 # Agg
-d.agg.group = d.agg[, .(value = sum(value),
-                        cons = sum(cons)), by = .(country, date, source.group)]
+d.agg.group = d.agg[, .(
+    value = sum(value),
+    cons = sum(cons)
+), by = .(country, date, source.group)]
 # Store
-saveToStorages(
-    d.agg.group,
-    list(
-        id = "electricity-generation-g1",
-        source = "entsoe",
-        format = "csv",
-        update.time = update.time
-    )
-)
+saveToStorages(d.agg.group, list(
+    id = "electricity-generation-g1",
+    source = "entsoe",
+    format = "csv",
+    update.time = update.time
+))
 
 # - GROUP 2
 addGroupCol(d.agg, c.sourceGroups2, nameOthers = nameOthers)
 # Agg
-d.agg.group = d.agg[, .(value = sum(value),
-                        cons = sum(cons)), by = .(country, date, source.group)]
+d.agg.group = d.agg[, .(
+    value = sum(value),
+    cons = sum(cons)
+), by = .(country, date, source.group)]
 # Store
-saveToStorages(
-    d.agg.group,
-    list(
-        id = "electricity-generation-g2",
-        source = "entsoe",
-        format = "csv",
-        update.time = update.time
-    )
-)
+saveToStorages(d.agg.group, list(
+    id = "electricity-generation-g2",
+    source = "entsoe",
+    format = "csv",
+    update.time = update.time
+))
